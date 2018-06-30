ec9034e67be8e1ef25c2004e7d2485c3dda2227f
Sophy <sophy.ouch@erudit.org>
2018-06-30 10:19:38 -0400

    Remove unused Ubuntu typeface

--- TraditionalThemePlugin.inc.php
+++ TraditionalThemePlugin.inc.php
@@ -36,11 +36,6 @@ class TraditionalThemePlugin extends ThemePlugin
 		$this->addMenuArea(array('primary', 'user'));
 
 		$this->addStyle(
-			'roboto',
-			'//fonts.googleapis.com/css?family=Ubuntu',
-			array('baseUrl' => 'https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet'));
-
-		$this->addStyle(
 			'cardo',
 			'//fonts.googleapis.com/css?family=Cardo',
 			array('baseUrl' => '<link href="https://fonts.googleapis.com/css?family=Cardo:400,400i,700" rel="stylesheet'));
