cc25479c44cb1bbc6ee56dd4f37075752e722d8c
Vitaliy Bezsheiko <vitaliybezsh@gmail.com>
2019-03-19 13:34:11 +0200

    Copyright year update

--- resources/app-debug.js
+++ resources/app-debug.js
@@ -17423,8 +17423,8 @@ this._trigger("afterTagRemoved",null,{tag:a,tagLabel:this.tagLabel(a)})}},remove
 /**
  * @file /js/main-theme.js
  *
- * Copyright (c) 2014-2018 Simon Fraser University
- * Copyright (c) 2000-2018 John Willinsky
+ * Copyright (c) 2014-2019 Simon Fraser University
+ * Copyright (c) 2000-2019 John Willinsky
  * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
  *
  * @brief Custom javascript functionality for Traditional OJS 3 theme plugin
