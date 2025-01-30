1a8667c5cf17b9439ff5e4433c81eb0fb6420729
Kaitlin Newson <kaitlin.newson@gmail.com>
2025-01-30 11:38:59 -0400

    fix plugin tests

--- .github/actions/tests.sh
+++ .github/actions/tests.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 set -e
-npx cypress run --spec "cypress/tests/data/10-ApplicationSetup/*.cy.js,cypress/tests/data/60-content/VkarbasizaedSubmission.cy.js"
-npx cypress run --config specPattern=plugins/themes/classic/cypress/tests/functional
-
+echo "Run cypress tests"
+npx cypress run  --config '{"specPattern":["plugins/themes/classic/cypress/tests/functional/*.cy.js"]}'
